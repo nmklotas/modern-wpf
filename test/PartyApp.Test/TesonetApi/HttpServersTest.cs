effd2d5e449db13bf4ec9b4e080abead0fad2719
nmklotas <nemoku@gmail.com>
2018-07-08 20:39:49 +0300

    cleanup

--- test/PartyApp.Test/TesonetApi/HttpServersTest.cs
+++ test/PartyApp.Test/TesonetApi/HttpServersTest.cs
@@ -6,22 +6,22 @@ using Xunit;
 
 namespace PartyApp.Test.TesonetApi
 {
-	public class HttpServersTest
-	{
-		[Fact]
-		public async Task FetchesServers()
-		{
-		    var servers = await CreateSut();
-		    (await servers.Fetch(new CancellationToken())).Should().NotBeEmpty();
-		}
+    public class HttpServersTest
+    {
+        [Fact]
+        public async Task FetchesServers()
+        {
+            var servers = await CreateSut();
+            (await servers.Fetch(new CancellationToken())).Should().NotBeEmpty();
+        }
 
         private static async Task<IServers> CreateSut()
         {
-	        var servers = await new HttpTesonetApi().Login(
-	            new Credentials("tesonet", "partyanimal"),
-	            new CancellationToken());
+            var servers = await new HttpTesonetApi().Login(
+                new Credentials("tesonet", "partyanimal"),
+                new CancellationToken());
 
-	        return servers;
+            return servers;
         }
-	}
+    }
 }
